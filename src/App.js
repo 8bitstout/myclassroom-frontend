19f0093862f71584ea2a39fcf96c49cf21db2615
Conor J Murphy <cscjm@wakefern.com>
2019-10-02 17:31:32 -0400

    added material-ui library

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import Card from './Components/Card';
-import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-const StyledRoot = styled.div`
-  padding: 50px 12px;
-`
-
-const StyledContainer = styled.div`
-  max-width: 550px;
-  width: 100%;
-  margin: auto;
-`
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 400
+  },
+});
 
 function App() {
   const [resources, setResources] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     fetch('/api/v1/resource')
@@ -27,20 +33,34 @@ function App() {
   }, [resources]);
 
   return (
-    <div className="App">
-      <div>
-        {resources.map(({ name, author, url }) => (
-          <StyledRoot>
-            <StyledContainer>
-              <Card
-                title={(<a href={url}>{name}</a>)}
-                description={`${author.firstName} ${author.middleName} ${author.lastName}`}
+    <Grid container spacing={24}>>
+      {resources.map(({ name, author, imageUrl, url }) => (
+        <Grid item md={3}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                image={imageUrl}
+                title={name}
+                className={classes.media}
               />
-            </StyledContainer>
-          </StyledRoot>
-        ))}
-      </div>
-    </div>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {author.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                <a href={url}>Get Resource</a>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
