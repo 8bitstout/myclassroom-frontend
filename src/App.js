6f68ab0403087379c6e6952242896293dec75ee3
Conor J Murphy <cscjm@wakefern.com>
2019-10-07 17:04:59 -0400

    added course list

--- src/App.js
+++ src/App.js
@@ -1,66 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 345,
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
   },
-  media: {
-    height: 400
+  inline: {
+    display: 'inline',
   },
-});
+}));
 
 function App() {
-  const [resources, setResources] = useState([]);
+  const [courses, setCourses] = useState([]);
   const classes = useStyles();
 
   useEffect(() => {
-    fetch('/api/v1/resources')
+    fetch('/api/v1/courses')
       .then(res => res.json())
       .then(response => {
         console.log(response);
-        setResources(response);
+        setCourses(response);
       })
       .catch(error => console.log(error));
-  }, [resources]);
+  }, [courses]);
 
   return (
-    <Grid container spacing={24}>>
-      {resources.map(({ name, author, imageUrl, url }) => (
-        <Grid item md={3}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                image={imageUrl}
-                title={name}
-                className={classes.media}
+    <Container maxWidth="sm">
+      <List className={classes.root}>
+        {courses.map(course => (
+          <>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar />
+              </ListItemAvatar>
+              <ListItemText
+                primary={course.name}
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      component="span"
+                      variant="body2"
+                      className={classes.inline}
+                      color="textPrimary"
+                    >
+                    Members: {course.members.length}
+                    </Typography>
+                  </React.Fragment>
+                }
               />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {author.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {name}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                <a href={url}>Get Resource</a>
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </>
+        ))}
+      </List>
+    </Container>
   );
 }
 
