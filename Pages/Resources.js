c34167cf6cb16a18404cbccc08c3518844694832
Conor J Murphy <cscjm@wakefern.com>
2019-10-07 16:48:15 -0400

    Moved app.js code to Resouces.js

---
+++ Pages/Resources.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 400
+  },
+});
+
+function Resources() {
+  const [resources, setResources] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetch('/api/v1/resources')
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        setResources(response);
+      })
+      .catch(error => console.log(error));
+  }, [resources]);
+
+  return (
+    <Grid container spacing={24}>>
+      {resources.map(({ name, author, imageUrl, url }) => (
+        <Grid item md={3}>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                image={imageUrl}
+                title={name}
+                className={classes.media}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {author.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {name}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                <a href={url}>Get Resource</a>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default Resources;
