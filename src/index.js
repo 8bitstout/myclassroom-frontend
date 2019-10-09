ad0037d1b4d2fc87ab02b56febe7677a1c6e3400
Conor J Murphy <cscjm@wakefern.com>
2019-10-09 15:14:35 -0400

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
 } from "react-router-dom";
 
 ReactDOM.render(
