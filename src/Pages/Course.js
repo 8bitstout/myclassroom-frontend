032df79730877d29f81ac8a7edbae56ae509fba3
Conor J Murphy <cscjm@wakefern.com>
2019-10-09 17:14:40 -0400

    changed Course page to make api call to get course by slug rather than using passed props

--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 
 function Course(props) {
-  const course = props.location.state;
+  const { courseSlug } = props.match.params;
+  const [course, setCourse] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/v1/courses/${courseSlug}`)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        setCourse(response);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
   return (
     <Container maxWidth="sm">
