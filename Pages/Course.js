1c107a49fb9b8f697f02d8de4c1fe189a01accf2
Conor J Murphy <cscjm@wakefern.com>
2019-10-09 15:23:34 -0400

    removed unused imports

--- Pages/Course.js
+++ Pages/Course.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import Container from '@material-ui/core/Container';
-import CourseListItem from './Components/CourseListItem';
 
 const useStyles = makeStyles(theme => ({
   root: {
